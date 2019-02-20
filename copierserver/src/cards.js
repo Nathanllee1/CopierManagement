ce1594f32ccaeab6a654d72b2414ccae89e0ca28
Nathan Lee <nathan.l.lee123@gmail.com>
2019-02-20 14:20:18 -0800

    Started work on React

---
+++ copierserver/src/cards.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Card from 'react-bootstrap';
+
+render() {
+  return(
+    <div class='card'>
+        <h5 class='card-title'>Engineering Room</h5>
+    </div>
+  )
+}
+
+
+
+export default PrinterCard;
