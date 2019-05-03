402133b19f76c8d122eafc8d327458f6f1df545e
Nathan Lee <nathan.l.lee123@gmail.com>
2019-05-02 17:53:24 -0700

    Kept working on samplepnger

--- samplepinger.py
+++ samplepinger.py
@@ -1,14 +1,50 @@
 import requests
+from bs4 import BeautifulSoup
+from requests.exceptions import HTTPError
 
-baseURL = 'http://10.99.17.50'
+state = {
+    '106A Copier 1' : {
+        'ip' : '10.99.17.50',
+        'status' : '',
+        'queue' : [],
+        'type' : 'bizhub'
+    },
+}
 
 s = requests.Session()
-payload = {'func':'PSL_LP0_TOP', 'AuthType':'None', 'TrackType':'Password', 'ExtSvType&Lang':'Auto', 'Mode':'Track', 'trackpassword':'password', 'ViewMode':'html', 'ShowDialog':'Dialog'}
-shortenedPayload = {'trackpassword' : 'password'}
-originalPayload = 'func=PSL_LP0_TOP&AuthType=None&TrackType=Password&ExtSvType=&Lang=En&Mode=Track&trackpassword=password&ViewMode=Html&ShowDialog=Dialog'
-login = s.post('http://10.99.17.50/wcd/top.xml', data=originalPayload)
+
+def login(ip):
+    payload = 'func=PSL_LP0_TOP&AuthType=None&TrackType=Password&ExtSvType=&Lang=En&Mode=Track&trackpassword=password&ViewMode=Html&ShowDialog=Dialog'
+    try:
+        s.post('http://' + ip + '/wcd/ulogin.cgi', data='payload')
+    except HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')  # Python 3.6
+    except Exception as err:
+        print(f'Other error occurred: {err}')  # Python 3.6
+    else:
+        return True
+
+def getQueue(ip):
+    dataURL = 'http://' + ip + '/wcd/job.xml'
+    data = s.get(dataURL)
+
+for copier in state:
+    print(copier)
+    if copier['type'] == 'bizhub':
+        if login(copier['ip']):
+            copier['queue'] = getQueue(copier['ip'])
+        else:
+            copier['status'] = 'unavaliable'
+
+'''
+login = s.post('http://10.99.17.50/wcd/ulogin.cgi', data=originalPayload)
 #print(login)
-print(login.text)
-dataURL = 'http://10.99.17.50/wcd/flash_donejob.xml?cnt=11933021'
-data = s.get(dataURL)
-print(data.headers)
+print('Logged in')
+data = s.get(dataURL).content
+
+soup = BeautifulSoup(data, 'html.parser')
+#print(soup.prettify)
+rows = soup.find_all("tr")
+
+print(rows)
+'''
