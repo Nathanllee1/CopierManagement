e0d9fa75897b33559f24234eb1d6ba788e7aaf02
Nathan Lee <nathan.l.lee123@gmail.com>
2019-05-23 18:20:37 -0700

    Final

--- copierserverbackend/app.py
+++ copierserverbackend/app.py
@@ -6,6 +6,14 @@ import csv
 import datetime
 import pandas
 
+#forms
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+
+
 database = pandas.read_csv("database.csv").to_dict('records')
 
 s = requests.Session()
@@ -16,31 +24,18 @@ def generateETA(length): #take number of originals * copies
     return ETA
 
 def login(ip):
-    '''
     payload = 'func=PSL_LP0_TOP&AuthType=None&TrackType=Password&ExtSvType=&Lang=En&Mode=Track&trackpassword=password&ViewMode=Html&ShowDialog=Dialog'
     try:
-        s.post('http://' + ip + '/wcd/ulogin.cgi', 'payload', timeout=2)
+        s.post('http://' + str(ip) + '/wcd/ulogin.cgi', 'payload', timeout=2)
     except HTTPError as http_err:
         print('HTTP error occurred:' + str(http_err))  # Python 3.6
     except Exception as err:
         print('Other error occurred: ' + str(err))  # Python 3.6
     else:
         return True
-    '''
-    return True
 
-def getData(ip):
-    '''
-    print('Getting data from ' + ip)
-    results = ''
-    cookies = {'selno': 'En', 'vm': 'Html','wd': 'y','uatype': 'NN','lang': 'En','ID': 'cbadf4c56bebf949dddc37cd47f838b4',  'usr': 'J_ACT',}
-    headers = {'Connection': 'keep-alive','Cache-Control': 'max-age=0','Upgrade-Insecure-Requests': '1', 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36', 'DNT': '1', 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3','Referer': 'http://10.99.17.50/wcd/system.xml', 'Accept-Encoding': 'gzip, deflate', 'Accept-Language': 'en-US,en;q=0.9',}
-    try:
-        results = str(s.get('http://' + ip + '/wcd/job.xml', headers=headers, cookies=cookies, timeout=3).text)
-    except Exception as err:
-        print('Other error occurred: ' + str(err))  # Python 3.6
-    '''
-    results = open('job.xml', 'r')
+def getData(results):
+    print('Getting data')
     page = BeautifulSoup(results, features='xml')
     printObject = page.find_all('Job')
     queue = {}
@@ -53,7 +48,7 @@ def getData(ip):
         #add the formatting zero
         if len(minute) == 1:
             minute = '0' + minute
-        startTime = (hour + ':' + minute)
+        startTime = (str(hour) + ':' + str(minute))
         print(jobType, jobID, startTime)
 
         status = jobs.find('Status').text
@@ -71,6 +66,14 @@ def getData(ip):
 
 from flask import Flask, render_template
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+
+class reportForm(FlaskForm):
+    notes = StringField('Notes')
+    submit = SubmitField('Submit')
+
+
 
 @app.route("/")
 def main():
@@ -80,27 +83,55 @@ def main():
         copierObject['Queue'] = []
         if copierObject['Type'] == 'Bizhub':
             if login(copierObject['IP']):
+                print('actual ip')
                 print('Logged in to ' + str(copierObject['Name']))
-                queueData = getData(copierObject['IP'])
+                results = ''
+                cookies = {'selno': 'En', 'vm': 'Html','wd': 'y','uatype': 'NN','lang': 'En','ID': 'cbadf4c56bebf949dddc37cd47f838b4',  'usr': 'J_ACT',}
+                headers = {'Connection': 'keep-alive','Cache-Control': 'max-age=0','Upgrade-Insecure-Requests': '1', 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36', 'DNT': '1', 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3','Referer': 'http://10.99.17.50/wcd/system.xml', 'Accept-Encoding': 'gzip, deflate', 'Accept-Language': 'en-US,en;q=0.9',}
+                try:
+                    results = str(s.get('http://' + str(copierObject['IP']) + '/wcd/job.xml', headers=headers, cookies=cookies, timeout=3).text)
+                except Exception as err:
+                    print('Other error occurred: ' + str(err))  # Python 3.6
+
+                queueData = getData(results)
                 copierObject['Queue'].append(queueData)
                 copierObject['Status'] = 'connected'
                 copierObject['JobCount'] = len(copierObject['Queue'])
+                copierObject['ETA'] = '0'
                 print(queueData)
                 if len(queueData) != 0:
-                    fulltime = fulltime + float(queueData['eta'])
+                    #fulltime = fulltime + float(queueData['eta'])
                     print('Yes')
             else:
-                print('unavaliable')
+                copierObject['Queue'] = ''
+                copierObject['JobCount'] = '0'
                 copierObject['Status'] = 'unavaliable'
+                copierObject['ETA'] = '0'
 
-    rawTime = str(datetime.timedelta(seconds=fulltime))
-    copierObject['ETA'] = rawTime
+        elif copierObject['Type'] == 'Test':
+            print('Recieved Test')
+            results = open('job.xml')
+            queueData = getData(results)
+            copierObject['Queue'].append(queueData)
+            copierObject['Status'] = 'connected'
+            copierObject['JobCount'] = len(copierObject['Queue'])
+            print(queueData)
+
+
+            rawTime = '24 Seconds'
+            copierObject['ETA'] = rawTime
 
 
     print(database)
-    return render_template('index.html', state=database)
+    form = reportForm()
+
+    if form.validate_on_submit():
+        copierObject['Status'] = 'Broken'
+        copierObject['brokenNotes'] = form.Notes.data
+        return redirect('/')
+    return render_template('index.html', state=database, form=form)
 
 
 if __name__ == "__main__":
     app.debug = True
-    app.run(host = '10.99.114.234', port=5000)
+    app.run(host = '10.99.116.132', port=5000)
